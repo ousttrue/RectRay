233bd69039a26d9a93357a8810a29216676f075b
ousttrue <ousttrue@gmail.com>
2023-07-25 14:37:08 +0900

    gles3

--- examples/glfw_imgui/gl_api.h
+++ examples/glfw_imgui/gl_api.h
@@ -9,17 +9,19 @@
 #include <vector>
 
 #ifdef __EMSCRIPTEN__
+#define GL_GLEXT_PROTOTYPES
 #include <imgui_impl_opengl3.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
-#endif
-#define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2ext.h>
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+#include <GLES3/gl3.h>
+#include <GLES3/gl3ext.h>
+#endif
 #define glBindVertexArray glBindVertexArrayOES
 #define glGenVertexArrays glGenVertexArraysOES
 #define glDeleteVertexArrays glDeleteVertexArraysOES
-#include <GLES3/gl3.h>
 inline const char *SHADER_HEADER = "#version 300 es\nprecision highp float;\n";
 
 #else
